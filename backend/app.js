66bfca1ea96efb93571a2ea618a9fe0bcf47e778
DTron <darrenstorey87@gmail.com>
2024-11-02 23:18:44 +0000

    Deploy to Heroku

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,22 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 const apiRoutes = require('./routes/api');
 const logger = require('./utils/logger');
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
 app.use('/api', apiRoutes);
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, '../frontend/build')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../frontend/build/index.html'));
+});
 
 app.listen(PORT, () => {
   logger.info(`Server is running on port ${PORT}`);
